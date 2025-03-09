11b1b37820f5de46532b70bc88fb57804b9a57a3
Ligadata-YazeedHasan <yazeed.hasan@ligadata.com>
2025-03-10 02:01:44 +0300

    error on timeout

--- controllers/screeners.py
+++ controllers/screeners.py
@@ -31,12 +31,12 @@ class NameScreener:
         _model_name = "NECOUDBFM/Jellyfish-13B"
         self.tokenizer = AutoTokenizer.from_pretrained(
             _model_name,  use_auth_token=use_auth_token,
-            timeout=60,  # Increase timeout to 60 seconds
+            # timeout=60,  # Increase timeout to 60 seconds
             resume_download=True,  # Resume failed downloads instead of restarting
         )
         self.model = AutoModel.from_pretrained(
             _model_name, use_auth_token=use_auth_token,
-            timeout=60,  # Increase timeout to 60 seconds
+            # timeout=60,  # Increase timeout to 60 seconds
             resume_download=True,  # Resume failed downloads instead of restarting
         )
 
